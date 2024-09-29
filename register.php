084efcbd32c776f1a0636b9a9ae24c76eade053c
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-09-29 21:01:05 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<p>Register here</p>
+	<form action="handleForm.php" method="POST">
+		<div class="fields">
+			<p><input type="text" placeholder="username here" class="fields" name="username"></p>
+			<p><input type="password" placeholder="password here" class="fields" name="password"></p>
+			<p><input type="submit" value="Register" id="submitBtn" name="regBtn"></p>
+		</div>
+	</form>
+</body>
+</html>
